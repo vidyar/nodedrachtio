409159c8087a2e9c974626aeddffee8b5b326fe4
Dave Horton <daveh@beachdognet.com>
2014-02-20 14:29:40 -0500

    initial working version of mks

--- test/b2bua.js
+++ test/b2bua.js
@@ -12,11 +12,12 @@ app.connect({
 }) ;
 
 app.use( drachtio.session({store: new RedisStore({host: 'localhost'}) }) ) ;
-app.use( drachtio.dialog() ) ;
+app.use( drachtio.dialog({autosave: false}) ) ;
 app.use( app.router ) ;
 
 app.invite(function(req, res) {
 
+    req.session.startTime = new Date() ;
     /* send new INVITE back to sender */
     var gotResponse = false ;
     siprequest( req.msg.source_address + ':' + req.msg.source_port, {
@@ -27,18 +28,6 @@ app.invite(function(req, res) {
     }, function( err, invite, uacRes ) {
 
         if( err ) throw( err ) ;
-
-        /*
-        if( !gotResponse ) {
-            req.sessionStore.set( req.get('call_id'), {
-                otherCallId: invite.get('call_id')
-            }) ;
-            req.sessionStore.set( invite.get('call_id'), {
-                otherCallId: rqe.get('call_id')
-            }) ;
-            gotResponse = true ;
-        }
-        */
 
         debug('received response to uac invite with status code %d', uacRes.statusCode ) ;
         res.send( uacRes.statusCode, {
@@ -52,26 +41,29 @@ app.invite(function(req, res) {
 
 app.on('sipdialog:create', function(e) {
     var dialog = e.target ;
+    var session = e.session ;
+
+    session[dialog.role === SipDialog.UAC ? 'uacLeg': 'uasLeg'] = dialog ;
+    session.username = 'daveh' ;
+    e.sessionSave() ;
 
     debug('sipdialog:create handler for dialog with id %s', dialog.dialogID) ;
 })
 .on('sipdialog:terminate', function(e) {
     var dialog = e.target ;
+    var session = e.session ;
+
+    debug('release came on %s dialog', dialog.role ) ;
+    if( dialog.role === SipDialog.UAS ) {
+        session.uacLeg.terminate() ;
+    }
+    else {
+        session.uasLeg.terminate() ;
+    }
+
+    var duration = new Date() - session.startTime() ;
     
     debug('dialog with role %s and dialogID %s was terminated due to %s', dialog.role, dialog.dialogID, e.reason ) ;
-/*
-    dialog.sessionStore.get( dialog.call_id, function( err, otherCallId){
-        if( err ) throw( err ) ;
-        debug('matching callid we need to send BYE on is %s', otherCallId) ;
-
-        SipDialog.loadFromSessionStore( dialog.sessionStore, otherCallId, dialog.role === 'uac' ? 'uas': 'uac', function( err, dlg){
-            if( err ) throw( err ) ;
-
-            debug('terminating dialog ', dlg) ;
-            dlg.terminate() ;
-        })
-    })
-*/
 }) ;
 
 
---
+++ test/mks-test.js
@@ -0,0 +1,68 @@
+var MKSession = require('../lib/middleware/session/multikeysession')
+drachtio = require('..')
+,RedisStore = require('drachtio-redis')(drachtio) 
+,uuid = require('node-uuid')
+,async = require('async')
+,assert = require('assert')
+,Resource = require('../lib/Resource')
+,util = require('util')
+,debug = require('debug')('drachtio:test') ;
+
+
+//var store = new RedisStore({host: 'localhost'}) ;
+
+var session = new MKSession({
+	//store: store
+	host: 'localhost'
+	,ttl: 60
+	,prefix: ''
+}) ;
+
+var store = session.store ;
+
+function Endpoint(opts) {
+	Resource.call( this, opts ) ;
+
+	this.codec = 'g711' ;
+}
+util.inherits(Endpoint, Resource);
+Endpoint.prototype.hello = function() {debug('Endpoint says hello, this: ', this);}
+
+Endpoint.prefix = 'ep' ;
+
+var ep = new Endpoint( uuid.v1() ) ;
+var ep2 = new Endpoint( uuid.v1() ) ;
+
+session.add( ep ) ;
+session.add( ep2 ) ;
+session.session = {
+	user: 'daveh'
+	,foo: 'bar'
+	,baz: function() {}
+	,ep: ep2 
+	,myData: {
+		endpoint: ep
+		,hash: Object.create(null)
+	}
+} ;
+session.save( function( err, res ) {
+	if( err ) throw err ;
+
+	MKSession.loadFromStorage({
+		store: store
+		,resolvers: [ Endpoint ]
+	}, ep.sessionID, function(err, mks){
+		if( err ) throw err ;
+
+		debug('retrieved mks ', mks) ;
+		debug('comparing to session ', session) ;
+
+		debug('mks.session.ep.hello() ', mks.session.ep.hello()) ;
+		debug('mks.session.myData.endpoint.hello() ', mks.session.myData.endpoint.hello()) ;
+
+		session.close() ;
+	}) ;
+}) ;
+
+return ;
+
